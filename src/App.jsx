6abf216550ba189e582e8fa678adf970bb331b4b
Didariiuc <c223413@ugrad.iiuc.ac.bd>
2025-01-17 11:15:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import './App.css'
+import navbar from './components/navbar/navbar'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const navbar = () => {
+    return (
+        <div>
+            <h1>PH Tube</h1>
+            <button>Sort by View</button>
+            <button>Blog</button>
+            
+        </div>
+    );
+};
+
+export default navbar;
